9864484f7feb19e5a7d7531a677f8efc3fe11673
Rodrigo <rodrigo.cardoso.oliveira@gmail.com>
2022-08-25 13:39:39 -0300

    Conversão de tipos

---
+++ src/fundamentos/operadores/Destructuring.kt
@@ -0,0 +1,19 @@
+package fundamentos.operadores
+
+data class Carro(val marca: String, val modelo: String)
+
+fun main(args: Array<String>) {
+    val(marca, modelo) = Carro("Ford", "Fusion")
+
+    println(marca)
+    println(modelo)
+
+
+    val (marido, mulher) = listOf("João", "Maria")
+
+    println("$marido e $mulher")
+
+    val(_, _, terceiroLugar) = listOf("Kimi", "Hamilton", "Alonso")
+
+    println("$terceiroLugar terminou em terceiro lugar")
+}
---
+++ src/fundamentos/operadores/Ternario.kt
@@ -0,0 +1,11 @@
+package fundamentos.operadores
+
+fun main(args: Array<String>) {
+
+    val nota: Double = 6.0
+
+    val resultado: String = if(nota>= 7) "Aprovado" else "Reprovado"
+
+    println(resultado)
+
+}
